9d93bebce8f2c0960b1fa8dce558b41ac435772d
Viktor <noki88@gmail.com>
2012-05-09 00:02:42 +0200

    add sheet2 m files

--- Mobile Robotics I/Sheet2/ex3b.m
+++ Mobile Robotics I/Sheet2/ex3b.m
@@ -2,5 +2,9 @@ scan = load("-ascii","laserscan.dat");
 angle = linspace(-pi/2,pi/2,size(scan,2));
 x1 = scan .* cos(angle);
 y1 = scan .* sin(angle);
-plot(x1, y1,'.')
+plot(1,0.5,'@1')
+hold on
+laser = X(1,0.5,(pi)/4)*[0.2;0;1];
+plot(laser(1,1),laser(2,1),'@2')
+hold off
 print('ex3b.png', '-dpng')
