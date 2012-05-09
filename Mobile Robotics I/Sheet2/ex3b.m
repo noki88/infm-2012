237d78c6936cdf64db199dd4fd2f9a40b071ebdd
Stefan Wrobel <stefan.w.ggb@gmail.com>
2012-05-09 13:54:38 +0200

    plot ergänzt

--- Mobile Robotics I/Sheet2/ex3b.m
+++ Mobile Robotics I/Sheet2/ex3b.m
@@ -4,7 +4,15 @@ x1 = scan .* cos(angle);
 y1 = scan .* sin(angle);
 plot(1,0.5,'@1')
 hold on
-laser = X(1,0.5,(pi)/4)*[0.2;0;1];
-plot(laser(1,1),laser(2,1),'@2')
+X_roboter = X(1,0.5,(pi)/4);
+X_laser = X(0.2,0,(pi));
+pos_laser = X_roboter*[0.2;0;1];
+plot(pos_laser(1,1),pos_laser(2,1),'@3')
+
+for i = 1:size(scan,2)
+  pos_res = X_roboter*X_laser*[x1(i);y1(i);1];
+  plot(pos_res(1,1),pos_res(2,1),'o5')
+endfor
+  
 hold off
 print('ex3b.png', '-dpng')
